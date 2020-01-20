0bdae3e4a8b5a96987081eb3f1b171beca5f63c4
Chris Philbin <chris@chrisphilbin.net>
2020-01-20 07:47:09 -0500

    importing provider and configstore and setting store variable

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,19 +1,24 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { BrowserRouter, Switch, Route } from "react-router-dom"
-
+import { Provicer } from 'react-redux'
 import HelloWorld from './HelloWorld'
+import configureStore from '../configureStore'
+
+const store = configureStore()
 
 
 class App extends React.Component {
   render () {
     return (
-      <ReactRouter>
-      	<Switch>
-      		<Route exact path="/" render{ () => ("Home!")} />
-      		<Route exact path="/hello" render{ () => <HelloWorld greeting="Friend"/> } />
-      	</Switch
-      </ReactRouter>
+    	<Provider store={store}>
+	      <ReactRouter>
+	      	<Switch>
+	      		<Route exact path="/" render{ () => ("Home!")} />
+	      		<Route exact path="/hello" render{ () => <HelloWorld greeting="Friend"/> } />
+	      	</Switch
+	      </ReactRouter>
+	    </Provider>
     );
   }
 }
