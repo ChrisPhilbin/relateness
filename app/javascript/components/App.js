12c73ba689671fd799972d546d24f4c87da73947
Chris Philbin <chris@chrisphilbin.net>
2020-01-21 21:04:25 -0500

    Fixing syntax errors

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -3,7 +3,7 @@ import PropTypes from "prop-types"
 import { BrowserRouter, Switch, Route } from "react-router-dom"
 import { Provider } from 'react-redux'
 import HelloWorld from './HelloWorld'
-import configureStore from './configureStore'
+import configureStore from '../configureStore'
 
 const store = configureStore()
 
@@ -14,8 +14,8 @@ class App extends React.Component {
     	<Provider store={store}>
 	      <BrowserRouter>
 	      	<Switch>
-	      		<Route path="/" render{ () => ("Home!")} />
-	      		<Route path="/hello" render{ () => <HelloWorld greeting="Friend"/> } />
+	      		<Route exact path="/" render={ () => ("Home!")} />
+	      		<Route path="/hello" render={ () => <HelloWorld greeting="Friend"/> } />
 	      	</Switch>
 	      </BrowserRouter>
 	    </Provider>
