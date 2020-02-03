7f5977908ed8ad5c6a3983dd73b0f5d47c812320
Chris Philbin <chris@chrisphilbin.net>
2020-02-03 07:56:20 -0500

    Adding index action to return json of all employees owned by user if they are signed in

--- app/controllers/v1/employees_controller.rb
+++ app/controllers/v1/employees_controller.rb
@@ -1,2 +1,11 @@
-class EmployeesController < ApplicationController
+class V1::EmployeesController < ApplicationController
+
+	def index
+		if user_signed_in?
+			render json: current_user.employees
+		else
+			render json: {}, status: 401
+		end
+	end
+	
 end
