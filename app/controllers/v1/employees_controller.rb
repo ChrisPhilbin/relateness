0fb4d3ce41604cc788e7032f02e9264bfb692837
Chris Philbin <chris@chrisphilbin.net>
2020-02-05 16:23:59 -0500

    Adding create action within employee controller - will return json object of created employee, else it'll return an error

--- app/controllers/v1/employees_controller.rb
+++ app/controllers/v1/employees_controller.rb
@@ -8,6 +8,18 @@ class V1::EmployeesController < ApplicationController
 		end
 	end
 
+	def create
+		if user_signed_in?
+			if employee = current_user.employees.create(employee_params)
+				render json: employee, status :created
+			else
+				render json: employee.errors, status: 400
+			end
+		else
+			render json: {}, status: 401
+		end	
+	end
+
 	private
 
 	def employee_params
