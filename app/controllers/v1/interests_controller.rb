b0bf6ddbc5ff2133abf1efb51442dc726d4d1aaf
Chris Philbin <chris@chrisphilbin.net>
2020-02-13 08:10:17 -0500

    Getting new interest form to work - added controller action to persist new interest objects to the DB

--- app/controllers/v1/interests_controller.rb
+++ app/controllers/v1/interests_controller.rb
@@ -7,4 +7,22 @@ class V1::InterestsController < ApplicationController
 			render json: {}, status: 401
 		end
 	end
+
+	def create
+		if user_signed_in?
+			if interest = Interest.create(interest_params)
+				render json: interest, status: :created
+			else
+				render json: interest.errors, status: 400
+			end
+		else
+			render json: {}, status: 401
+		end	
+	end
+
+	private
+
+	def interest_params
+		params.require(:interest).permit(:interest)
+	end
 end
