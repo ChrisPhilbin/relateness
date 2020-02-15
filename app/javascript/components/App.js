39129c7ae1062b5688ebbcab2b1e70d09a033919
Chris Philbin <chris@chrisphilbin.net>
2020-02-15 15:42:15 -0500

    Rendering EmployeeList and InterestList components to the DOM via the MainPage component

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -2,9 +2,8 @@ import React from "react"
 import PropTypes from "prop-types"
 import { BrowserRouter, Switch, Route } from "react-router-dom"
 import { Provider } from 'react-redux'
-import EmployeeList from './employee/EmployeeList'
+import MainPage from './MainPage'
 import NewEmployee from './employee/NewEmployee'
-import InterestList from './interest/InterestList'
 import NewInterest from './interest/NewInterest'
 import configureStore from '../configureStore'
 
@@ -17,7 +16,7 @@ class App extends React.Component {
     	<Provider store={store}>
 	      <BrowserRouter>
 	      	<Switch>
-	      		<Route exact path="/" render={ () => <EmployeeList /> } />
+	      		<Route exact path="/" render={ () => <MainPage /> } />
 	      		<Route exact path="/interests" render={ () => <InterestList /> } />
 	      		<Route exact path="/interests/new" render={ () => <NewInterest /> } />
 	      		<Route exact path="/employees/new" render={ () => <NewEmployee /> } />
--- app/javascript/components/MainPage.js
+++ app/javascript/components/MainPage.js
@@ -1,8 +1,18 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import EmployeeList from './employee/EmployeeList'
+import InterestList from './interest/InterestList'
 
 class MainPage extends Component {
 
+	render() {
+		return (
+			<div>
+				<EmployeeList />
+				<InterestList />
+			</div>
+		)
+	}
 }
 
 export default MainPage
