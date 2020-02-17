b0dcd177e89a9f21676378efbeb88ed4ae63ecf4
Chris Philbin <chris@chrisphilbin.net>
2020-02-17 07:58:42 -0500

    Adding top navbar which inclues links to help user navigate through the app

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -3,6 +3,7 @@ import React from "react"
 import PropTypes from "prop-types"
 import { BrowserRouter, Switch, Route } from "react-router-dom"
 import { Provider } from 'react-redux'
+import TopNav from './navigation/TopNav'
 import MainPage from './MainPage'
 import NewEmployee from './employee/NewEmployee'
 import NewInterest from './interest/NewInterest'
@@ -14,16 +15,19 @@ const store = configureStore()
 class App extends React.Component {
   render () {
     return (
-    	<Provider store={store}>
-	      <BrowserRouter>
-	      	<Switch>
-	      		<Route exact path="/" render={ () => <MainPage /> } />
-	      		<Route exact path="/interests" render={ () => <InterestList /> } />
-	      		<Route exact path="/interests/new" render={ () => <NewInterest /> } />
-	      		<Route exact path="/employees/new" render={ () => <NewEmployee /> } />
-	      	</Switch>
-	      </BrowserRouter>
-	    </Provider>
+    	<div>
+	    	<Provider store={store}>
+		      <BrowserRouter>
+		      	<TopNav />
+		      	<Switch>
+		      		<Route exact path="/" render={ () => <MainPage /> } />
+		      		<Route exact path="/interests" render={ () => <InterestList /> } />
+		      		<Route exact path="/interests/new" render={ () => <NewInterest /> } />
+		      		<Route exact path="/employees/new" render={ () => <NewEmployee /> } />
+		      	</Switch>
+		      </BrowserRouter>
+		    </Provider>
+		</div>
     );
   }
 }
--- app/javascript/components/navigation/TopNav.js
+++ app/javascript/components/navigation/TopNav.js
@@ -1,8 +1,34 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 
-const TopNav = () => {
-
-}
+const TopNav = () => (
+  <header>
+	<nav className="navbar navbar-expand-lg navbar-light bg-light">
+	  <a className="navbar-brand" href="/">Relateness - Connect with your peers</a>
+	  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+	    <span className="navbar-toggler-icon"></span>
+	  </button>
+	  <div className="collapse navbar-collapse" id="navbarNav">
+	    <ul className="navbar-nav">
+	      <li className="nav-item active">
+	        <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
+	      </li>
+	      <li className="nav-item">
+	        <NavLink to ="#" className="nav-link">Employees</NavLink>
+	      </li>
+	      <li className="nav-item">
+	      	<NavLink to="#" className="nav-link">Interests</NavLink>
+	      </li>
+	      <li className="nav-item">
+	      	<NavLink to="#" className="nav-link">Help</NavLink>
+	      </li>
+	      <li className="nav-item">
+	        <a className="nav-link disabled" href="#">Signout</a>
+	      </li>
+	    </ul>
+	  </div>
+	</nav>
+  </header>
+)
 
 export default TopNav
