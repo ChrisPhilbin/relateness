1d567d91162dabf402b709d1f5b66710d80a9c4c
Chris Philbin <chris@chrisphilbin.net>
2020-02-26 21:02:28 -0500

    Adding componentDidMount function

--- app/javascript/components/employee/EditEmployee.js
+++ app/javascript/components/employee/EditEmployee.js
@@ -6,6 +6,21 @@ class EditEmployee extends Component {
 		employee: null
 	}
 
+	componentDidMount() {
+
+		const { id } = this.props.match.params
+
+		fetch('/v1/employees/' + id)
+		.then(employee_details => employee_details.json())
+		.then((employee_details) => {
+			this.setState(() => ( { employee: employee_details }))
+		})
+	}
+
+	onChange(e) {
+
+	}
+
 }
 
 export default EditEmployee
