2aa0ecc1278b0897e766dfea816a93480619de44
Chris Philbin <chris@chrisphilbin.net>
2020-03-30 07:58:59 -0400

    Implementing ability to show interests of an individual employee on their details page

--- app/javascript/components/employee/EmployeeDetails.js
+++ app/javascript/components/employee/EmployeeDetails.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import ShowInterest from './interest/ShowInterest'
 
 class EmployeeDetails extends Component {
 
 	state = {
-		employee: {}
+		employee: {},
+		interests: []
 	}
 
 	componentDidMount() {
@@ -16,20 +18,18 @@ class EmployeeDetails extends Component {
 		.then((employee_details) => {
 			this.setState(() => ( { employee: employee_details }))
 		})
-	}
 
-	renderEmployee = () => {
-		return(
-			<div>
-				{this.state.employee.fullname} <Link to={'/employees/' + this.state.employee.id + '/edit'}>Edit</Link><br />
-			</div>
-		)
+		fetch('/v1/employees/' + id + 'interests')
+		.then(employee_interests => employee_interests.json())
+		.then((employee_interests) => {
+			this.setState(() => ( { interests: employee_interests }))
+		})		
 	}
 
 	render() {
 		return(
 			<div>
-				{this.renderEmployee()}
+				{this.state.employee.fullname} <Link to={'/employees/' + this.state.employee.id + '/edit'}>Edit</Link><br />
 			</div>
 		)
 	}
