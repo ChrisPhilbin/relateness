980577cb8aad8f88f555dfecc07d09755aa77e15
Chris Philbin <chris@chrisphilbin.net>
2020-05-11 22:47:47 -0400

    Abstracting away displaying employee details

--- app/javascript/components/employees/EmployeeDetails.js
+++ app/javascript/components/employees/EmployeeDetails.js
@@ -1,15 +1,33 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import {useSelector, useDispatch} from 'react-redux'
+
+import {fetchSingleEmployee} from '../actions/employeesActions'
+
 import Alert from 'react-bootstrap/Alert'
 
-export const EmployeeDetails = ({employee_details}) => (
-	<div key={employee_details.id}>
-		<br />
-		<Alert key={employee_details.id} variant="primary">
-			<strong>Viewing {employee_details.fullname}'s details</strong>
-		</Alert>
-		<strong>Work anniversary:</strong><br />
-		<strong>Birthday:</strong><br />
-		<strong>Children:</strong><br />
-		<strong>Pets:</strong>
-	</div>
-)
+export const EmployeeDetails = (props) => {
+
+
+	const dispatch = useDispatch()
+
+	const employee_details =         useSelector(state => state.employees.employee_details)
+	const employee_loading =         useSelector(state => state.employees.loading)
+	const employee_hasErrors =       useSelector(state => state.employees.hasErrors)
+
+	useEffect(() => {
+		dispatch(fetchSingleEmployee(props.id))
+	}, [])
+
+	return(
+		<div key={employee_details.id}>
+			<br />
+			<Alert key={employee_details.id} variant="primary">
+				<strong>Viewing {employee_details.fullname}'s details</strong>
+			</Alert>
+			<strong>Work anniversary:</strong><br />
+			<strong>Birthday:</strong><br />
+			<strong>Children:</strong><br />
+			<strong>Pets:</strong>
+		</div>
+	)
+}
--- app/javascript/components/employees/ShowEmployeeDetails.js
+++ app/javascript/components/employees/ShowEmployeeDetails.js
@@ -1,7 +1,4 @@
-import React, {useEffect} from 'react'
-import {useSelector, useDispatch} from 'react-redux'
-
-import {fetchSingleEmployee} from '../actions/employeesActions'
+import React from 'react'
 
 import {EmployeeDetails} from './EmployeeDetails'
 import {EmployeeInterest} from '../interests/EmployeeInterest'
@@ -9,26 +6,10 @@ import {DisplayInterestsNewsItems} from '../interests/DisplayInterestsNewsItems'
 
 const ShowEmployeeDetails = (props) => {
 
-	const dispatch = useDispatch()
-
-	const employee_details =         useSelector(state => state.employees.employee_details)
-	const employee_loading =         useSelector(state => state.employees.loading)
-	const employee_hasErrors =       useSelector(state => state.employees.hasErrors)
-
-	useEffect(() => {
-		dispatch(fetchSingleEmployee(props.id))
-	}, [])
-
-	const renderEmployeeDetails = () => {
-		if (employee_loading)   return <p>Loading employee details...</p>
-		if (employee_hasErrors) return <p>Something went wrong loading the employee details... please try again...</p>
-		return <EmployeeDetails employee_details={employee_details} />
-	}
-
 	return (
 		<section>
 			<div>
-				{renderEmployeeDetails()}
+				<EmployeeDetails id={props.id} />
 				<EmployeeInterest id={props.id} />
 				<DisplayInterestsNewsItems id={props.id} />
 			</div>
