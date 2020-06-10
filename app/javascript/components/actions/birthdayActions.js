41abf01856ac5d97b12ed1294d72d030f60bab82
Chris Philbin <chris@chrisphilbin.net>
2020-06-10 07:44:08 -0400

    Removing reduntant action creators and defining the fetchBirthdays function

--- app/javascript/components/actions/birthdayActions.js
+++ app/javascript/components/actions/birthdayActions.js
@@ -1,33 +1,32 @@
-export const GET_UPCOMING_BIRTHDAYS         = 'GET_UPCOMING_BIRTHDAYS'
-export const GET_UPCOMING_BIRTHDAYS_SUCCESS = 'GET_UPCOMING_BIRTHDAYS_SUCCESS'
-export const GET_UPCOMING_BIRTHDAYS_FAILURE = 'GET_UPCOMING_BIRTHDAYS_FAILURE'
-export const GET_TODAYS_BIRTHDAYS           = 'GET_TODAYS_BIRTHDAYS'
-export const GET_TODAYS_BIRTHDAYS_SUCCESS   = 'GET_TODAYS_BIRTHDAYS_SUCCESS'
-export const GET_TODAYS_BIRTHDAYS_FAILURE   = 'GET_TODAYS_BIRTHDAYS_FAILURE'
+export const GET_BIRTHDAYS         = 'GET_BIRTHDAYS'
+export const GET_BIRTHDAYS_SUCCESS = 'GET_BIRTHDAYS_SUCCESS'
+export const GET_BIRTHDAYS_FAILURE = 'GET_BIRTHDAYS_FAILURE'
 
 
-export const getUpcomingBirthdays = () => ({
+
+export const getBirthdays = () => ({
     type: GET_UPCOMING_BIRTHDAYS,
 })
 
-export const getUpcomingBirthdaysSuccess = (employees) => ({
+export const getBirthdaysSuccess = (employees) => ({
     type: GET_UPCOMING_BIRTHDAYS_SUCCESS,
     payload: employees,
 })
 
-export const getUpcomingBirthdaysFailure = () => ({
+export const getBirthdaysFailure = () => ({
     type: GET_UPCOMING_BIRTHDAYS_FAILURE,
 })
 
-export const getTodaysBirthdays = () => ({
-    type: GET_TODAYS_BIRTHDAYS,
-})
 
-export const getTodaysBirthdaysSuccess = (employees) => ({
-    type: GET_TODAYS_BIRTHDAYS_SUCCESS,
-    payload: employees,
-})
+export function fetchBirthdays() {
+	return (dispatch) => {
+		dispatch(getBirthdays());
+			fetch('/v1/employees/birthdays')
+				.then(response => response.json())
+				.then(data => dispatch(getBirthdaysSuccess(data)));
+	}
+}
 
-export const getTodaysBirthdaysFailure = () => ({
-    type: GET_TODAYS_BIRTHDAYS_FAILURE,
-})
+export function fetchTodaysBirthdays() {
+
+}
